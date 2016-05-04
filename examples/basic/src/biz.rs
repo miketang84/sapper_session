d515472dc57043148f0b4c876ed6b4f92098c91f
Mike Tang <miketang84@outlook.com>
2016-05-04 16:42:36 +0800

    work now.
    Signed-off-by: Mike Tang <miketang84@outlook.com>

---
+++ examples/basic/src/biz.rs
@@ -0,0 +1,75 @@
+
+use sapper::Result;
+use sapper::SModule;
+use sapper::Request;
+use sapper::Response;
+use sapper::SRouter;
+
+#[derive(Clone)]
+pub struct Biz;
+
+use sapper_cookie::SAppCookie;
+
+impl Biz {
+    // those handlers in module Biz
+    fn index(req: &mut Request) -> Result<Response> {
+        
+        let mut response = Response::new();
+        response.write_body("hello, boy!".to_string());
+        
+        Ok(response)
+    }
+    
+    fn test(req: &mut Request) -> Result<Response> {
+        println!("{:?}", req.ext().get::<SAppCookie>());
+        
+        let mut response = Response::new();
+        response.write_body("hello, tang gang gang!".to_string());
+        
+        Ok(response)
+    }
+    
+    fn test_post(req: &mut Request) -> Result<Response> {
+        
+        println!("in test_post, raw_body: {:?}", req.raw_body());
+        
+        let mut response = Response::new();
+        response.write_body("hello, I'am !".to_string());
+        
+        Ok(response)
+    }
+    
+}
+
+// set before, after middleware, and add routers
+impl SModule for Biz {
+    
+    fn before(&self, req: &mut Request) -> Result<()> {
+        println!("{}", "in Biz before.");
+        Ok(())
+    }
+    
+    fn after(&self, req: &Request, res: &mut Response) -> Result<()> {
+        println!("{}", "in Biz after.");
+        
+        Ok(())
+    }
+    
+    // here add routers ....
+    fn router(&self, router: &mut SRouter) -> Result<()> {
+        // need to use Router struct here
+        // XXX: here could not write as this, should record first, not parse it now
+        
+        
+        router.get("/", Biz::index);
+        router.get("/123", Biz::index);
+        router.get("/test", Biz::test);
+        router.post("/test", Biz::test_post);
+        
+        Ok(())
+        
+    }
+    
+    
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,64 @@
+
+
+extern crate sapper;
+
+use sapper::header::{Cookie, CookiePair};
+
+use sapper::{Request, Response, Result, Key};
+
+pub struct SAppCookie;
+impl Key for SAppCookie { type Value = String; }
+
+pub fn process(req: &mut Request, ckey: &str) -> Result<()> {
+    
+    let mut cookie_value: Option<String> = None;
+    match req.headers().get::<Cookie>() {
+        Some(ref value) => {
+            let Cookie(ref ckvec) = **value;
+            let cookie_vec = ckvec.iter()
+                                .filter(|item: &&CookiePair| item.name == ckey.to_owned())
+                                .take(1)
+                                .collect::<Vec<&CookiePair>>();
+            
+            if cookie_vec.len() == 1 {
+                let cookie_obj = cookie_vec[0];
+                cookie_value = Some(cookie_obj.value.clone());
+            }
+            
+            
+        },
+        None => {
+            println!("no cookie in headers");
+        }
+    }
+    
+    cookie_value.and_then(|val| {
+        req.ext_mut().insert::<SAppCookie>(val);
+        Some(())
+    });
+    
+    // if cookie_value.is_some() {
+    //     req.ext_mut().insert::<SAppCookieValue>(cookie_value.unwrap());
+    // }
+    
+    Ok(())
+}
+
+
+// pub fn make_uuid() -> String {
+    
+    
+// }
+
+// library function
+pub fn set_cookie(res: &mut Response) -> Result<()> {
+    
+    Ok(())
+}
+
+
+
+
 #[cfg(test)]
 mod tests {
     #[test]
